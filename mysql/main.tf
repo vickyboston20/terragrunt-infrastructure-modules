56282967a681b48d7773c8f7a26362e8d041120c
vignesh_devops_tf <v.thiyagarajan@perficient.com>
2022-04-01 13:41:24 +0530

    renamed name to db_name

--- mysql/main.tf
+++ mysql/main.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "mysql" {
   engine         = "mysql"
   engine_version = "5.6.41"
 
-  name     = var.name
+  db_name     = var.name
   username = var.master_username
   password = var.master_password
 
